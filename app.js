cce6602b1086b59e84e1c997e065b780f6652e10
alamin0561464977 <amin23435678901315@gmail.com>
2022-09-03 10:55:07 +0300

    display news detail

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ const displayNews = (news) => {
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('card', 'mb-3')
         newsDiv.innerHTML = `
-        <div onclick="lodeNewsDetail('${_id}')" class="row g-0">
+        <div data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="lodeNewsDetail('${_id}')" class="row g-0">
             <div class="col-12 col-sm-12 col-md-12 col-lg-4">
                 <img style="height: 100%;" src="${image_url}" class="img-fluid rounded-start" alt="...">
             </div>
@@ -76,10 +76,51 @@ const displayNews = (news) => {
     })
 };
 const lodeNewsDetail = (newsId) => {
-    // console.log(newsId);
+    console.log(newsId);
     fetch(`https://openapi.programming-hero.com/api/news/${newsId}`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayNewsDetail(data.data[0]))
+};
+const displayNewsDetail = (newsDetail) => {
+    console.log(newsDetail);
+    const { title, image_url, author, published_date, details, total_view } = newsDetail;
+    const { name, img } = author;
+    document.getElementById('modalTitle').innerText = title;
+    const modalBody = document.getElementById('modalBody');
+    modalBody.innerHTML = "";
+    const newsDiv = document.createElement('div');
+    newsDiv.classList.add('card', 'mb-3')
+    newsDiv.innerHTML = `
+        <div data-bs-toggle="modal" data-bs-target="#exampleModal" class="row g-0">
+            <div class="col-12">
+                <img style="height: 100%;" src="${image_url}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-12">
+                <div class="card-body">
+                    <p class="card-text">${details}</p>
+                    <div class="row">
+                        <div class="col-12 col-sm-12 col-md-5 col-lg-5 d-flex">
+                            <img style="max-width: 50px; height: 50px ;" src="${img}" class="img-fluid rounded-circle" alt="...">
+                            <div class="mx-2">
+                                <p class="mb-0">${name}</p>
+                                <p>${published_date}</p>
+                            </div>
+                        </div>
+                        <div class="col-2">
+                            <p><img style="max-width: 20px; height: 30px ;" src="https://iconarchive.com/download/i83709/custom-icon-design/mono-general-4/eye.ico" class="img-fluid rounded-circle" alt="..."> ${total_view}</p>
+                        </div>
+                        <div class="col-3">
+                            <img style="max-width: 100%; height: 60px ;" src="https://static.vecteezy.com/system/resources/thumbnails/003/355/389/small/five-5-star-rank-sign-illustration-free-vector.jpg" class="img-fluid rounded-circle" alt="...">
+                        </div>
+                        <div class="col-2">
+                            <img style="max-width: 100px; height: 80px ;" src="https://www.freeiconspng.com/thumbs/white-arrow-png/white-arrow-png-17.png" class="img-fluid rounded-circle" alt="...">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+    modalBody.appendChild(newsDiv);
 }
 
 
